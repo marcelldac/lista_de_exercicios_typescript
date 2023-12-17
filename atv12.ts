5e93821695b5cc0dac019237865da259595614a5
marcelldac <dacmarcell@gmail.com>
2023-12-17 16:39:27 -0300

    refactor: change variable scope

--- atv12.ts
+++ atv12.ts
@@ -1,15 +1,17 @@
 //Crie uma função que receba como parâmetro um array de números e retorne um array contendo somente números positivos.
 
-function retornarPositivos(ar: number[]): number[] {
-  var ar2: number[] = [];
-  for (var i = 0; i < ar.length; i++) {
-    var el: number = ar[i];
-    if (el >= 0) {
-      ar2.push(el);
+function retornarPositivos(arr: number[]): number[] {
+  let arr2: number[] = [];
+  for (let i = 0; i < arr.length; i++) {
+    let element: number = arr[i];
+    if (element >= 0) {
+      arr2.push(element);
     }
   }
-  return ar2;
+  return arr2;
+  ("");
 }
 
-var ar: number[] = [-5, 10, -3, 12, -9, 5, 90, 0, 1];
-var ar2: number[] = retornarPositivos(ar);
+let arr: number[] = [-5, 10, -3, 12, -9, 5, 90, 0, 1];
+let res: number[] = retornarPositivos(arr);
+console.log(res);
